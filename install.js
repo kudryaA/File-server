b408f66d0f8531dbabe8b00b3503b6629d4b68cd
Anton Kudryavtsev <kam123ua@gmail.com>
2019-01-12 23:10:36 +0200

    Add script for install

---
+++ install.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+let result = '';
+rl.question('Which port to use to start the server?\nport:', (port) => {
+  result += `port: ${port}`;
+  result += '\n';
+  rl.question('Which folder to use to sharing file(in format /home/kudrya/Documents)?\npath:', (path) => {
+    result += `path: ${path}`;
+    result += '\n';
+    fs.writeFileSync('configuration.yml', result);
+    rl.close();
+  });
+});
